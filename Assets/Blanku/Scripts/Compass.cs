ee77f641beaddfe96162664a7b74a29c8e145b49
JerryNie <nieshixin@gmail.com>
2018-02-15 04:09:03 -0500

    New Level, scene loading, destroy goo pool on scene loaded

---
+++ Assets/Blanku/Scripts/Compass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Compass : MonoBehaviour {
+	public Transform player;
+	public Transform Goal;
+
+	public Vector3 northDirection;
+	public Quaternion missionDirection;
+
+	public RectTransform northLayer;
+	public RectTransform missionLayer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateNorthDirection ();
+		UpdateMissionCompass ();
+		
+	}
+	public void UpdateNorthDirection(){
+		northDirection.z = player.eulerAngles.y;
+		northLayer.eulerAngles = northDirection;
+	}
+
+	public void UpdateMissionCompass(){
+		Vector3 dir = player.position - Goal.position;
+		Vector2 dir2 = new Vector2 (dir.x, dir.z);
+		float angle = Vector2.Angle (Vector2.down , dir2 );
+		//missionDirection = Quaternion.LookRotation (dir);
+
+		//missionDirection.z = -missionDirection.y;
+	//	missionDirection.x = 0;
+	//	missionDirection.y = 0;
+		//missionLayer.localRotation.z *= angle;
+		missionLayer.eulerAngles = new Vector3 (0f, 0f, angle);
+	}
+}
---
+++ Assets/Blanku/Scripts/NextLevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextLevelLoader : MonoBehaviour {
+	public string NextLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			LoadNextScene ();
+		}
+	}
+	public void LoadNextScene(){
+		SceneManager.LoadScene (NextLevel);
+		GameObject.Destroy(GameObject.Find ("Orange Goo Pool").gameObject);
+	}
+}
