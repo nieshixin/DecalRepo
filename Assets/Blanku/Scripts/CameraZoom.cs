ba2a62e757c85a51e526841f3885b7ca3e2d439d
JerryNie <nieshixin@gmail.com>
2018-02-16 18:11:57 -0500

    mouse wheel - camera zoom

---
+++ Assets/Blanku/Scripts/CameraZoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+	Camera cam; 	
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Mouse ScrollWheel")>0) {
+			cam.orthographicSize -= 0.2f;
+		}
+		if(Input.GetAxis ("Mouse ScrollWheel")<0)
+			cam.orthographicSize += 0.2f;
+	}
+}
---
+++ Assets/Blanku/Scripts/IllusionTeleport.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IllusionTeleport : MonoBehaviour {
+
+	GameObject playerRef;
+	FirstPersonCharacterController controller;
+	BoxCollider thisCol;
+	MeshRenderer mesh;
+	[Header("Available angle")][Range(1,4)]
+	public List<int> availableAngles;
+
+
+	// Use this for initialization
+	void Start () {
+		thisCol = GetComponent<BoxCollider> ();
+		mesh = GetComponent<MeshRenderer> ();
+		playerRef = GameObject.FindGameObjectWithTag("Player");
+		controller = playerRef.GetComponent<FirstPersonCharacterController> ();
+		//register the check function to the player, so everytime the player rotates the camera, this checks the illusion
+		controller.m_lookAngleChangeEvent.AddListener (CheckIllusion);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void CheckIllusion(int lookAngle){
+		Debug.Log (lookAngle);
+		thisCol.isTrigger = true;
+		mesh.enabled = false;
+		for (int i = 0; i < availableAngles.Count; i++) {
+			if (availableAngles [i] == lookAngle) {
+				thisCol.isTrigger = false;
+				mesh.enabled = true;
+			}
+		}
+
+	}
+}
