087f7a2aff80d2dae2464f095edcfb01151102dd
JerryNie <nieshixin@gmail.com>
2018-02-16 20:04:45 -0500

    added editor script to rotate camera angles when designing levels

---
+++ Assets/Blanku/Scripts/eidtorScript/levelDesignCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelDesignCamera : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public  void RotateLeft90(){
+		transform.Rotate (Vector3.up*90f, Space.World);
+	}
+	public void RotateRight90(){
+		transform.Rotate (Vector3.down*90f, Space.World);
+	}
+
+}
+
+
---
+++ Assets/Editor/levelDesignCameraEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(levelDesignCamera))]
+public class levelDesignCameraEditor : Editor {
+	
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector ();
+		levelDesignCamera myscript = (levelDesignCamera)target;
+		if (GUILayout.Button ("Left 90")) {
+			myscript.RotateLeft90 ();
+		}
+		if (GUILayout.Button ("Right 90")) {
+			myscript.RotateRight90 ();
+		}
+	}
+
+}
