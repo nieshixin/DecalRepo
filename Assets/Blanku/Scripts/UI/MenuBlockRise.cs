2e9ee363a8d355fe419c0ece7fae0215052ea60d
NUNET\nie.s <nieshixin@gmail.com>
2018-02-21 18:17:49 -0500

    start menu

---
+++ Assets/Blanku/Scripts/UI/MenuBlockRise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBlockRise : MonoBehaviour {
+	public float amount;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RandomMove(){
+		amount = Random.Range (3f,10f);
+		iTween.MoveTo (gameObject,iTween.Hash("y",amount, "time", 0.2f));
+		Debug.Log ("1");
+	}
+}
