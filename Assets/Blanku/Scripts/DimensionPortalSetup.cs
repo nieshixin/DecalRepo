49caf0b865ff441a7b36b9b672f9e2e6b614e6d5
JerryNie <nieshixin@gmail.com>
2018-03-19 06:11:45 -0400

    demension portal

---
+++ Assets/Blanku/Scripts/DimensionPortalSetup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionPortalSetup : MonoBehaviour {
+	public Camera camera_B;
+	public Material CamMat_B;
+	// Use this for initialization
+	void Start () {
+		if (camera_B.targetTexture != null) {
+			camera_B.targetTexture.Release ();
+		}
+		camera_B.targetTexture = new RenderTexture  (Screen.width, Screen.height, 24);
+		CamMat_B.mainTexture = camera_B.targetTexture;
+	}
+	
+
+}
---
+++ Assets/Blanku/Scripts/PortalCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+	public Transform playerCamera;
+	public Transform PortalPlane;
+	public Transform otherPortal; //ther portal which player is close to
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+		transform.position = PortalPlane.position + playerOffsetFromPortal;
+		float angleDiff = Quaternion.Angle (PortalPlane.rotation, otherPortal.rotation);
+		Quaternion rotationDiff = Quaternion.AngleAxis (angleDiff, Vector3.up);
+		Vector3 newCamDirection = rotationDiff * playerCamera.forward;
+		transform.rotation = Quaternion.LookRotation (newCamDirection, Vector3.up);
+	}
+}
