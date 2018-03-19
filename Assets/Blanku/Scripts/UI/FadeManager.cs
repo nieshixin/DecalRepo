65935d50f63ff768df5efd3d24bb01a80d5ed07f
JerryNie <nieshixin@gmail.com>
2018-03-19 03:52:55 -0400

    adding post processing

---
+++ Assets/Blanku/Scripts/UI/FadeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeManager : MonoBehaviour {
+	public static FadeManager instance{get; set;}
+
+	Image FadeImage;
+	bool isInTransition;
+	bool isShowing;
+	float duration;
+	float transition;
+
+	void Awake(){
+		instance = this;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
